e9b174cfddbe3ed8b307b2c9d911d1734a9e57f3
hyx0329 <hyx0329@163.com>
2021-07-01 17:59:48 +0800

    implement some tests

--- tests/test_parser_ll.py
+++ tests/test_parser_ll.py
@@ -4,15 +4,46 @@ from .data_to_test_ll import LLTestData as test_data_two
 
 
 def test_ll_no_recursion():
-    characters = set('LEGTHFIN;-+*/*()')
+    characters = set('LEGTHFIN;-+*/*()%')
     # characters.add('@')
     ll_parser = LLOne(test_data_one.raw_set_test,
                       word_list=characters,
                       start='L')
-    print()
-    for d in ll_parser.first_sets, ll_parser.follow_sets:
-        for k, v in zip(d.keys(), d.values()):
-            print(k, v)
-        print()
-    print()
-    print(ll_parser.parser._regexs)
+    mapper = ll_parser.parser.mapper
+
+    assert len(characters) == len(mapper)
+
+    result_first_sets = ll_parser.first_sets
+    result_follow_sets = ll_parser.follow_sets
+
+    converted_firsts = dict()
+    converted_follows = dict()
+
+    def map_to_real(x):
+        if isinstance(x, int):
+            if x < 0:
+                return ''
+            else:
+                return mapper[x]
+        else:
+            return x
+    
+    pairs = (
+        (result_first_sets, converted_firsts),
+        (result_follow_sets, converted_follows)
+    )
+
+    for source, target in pairs:
+        for key, value in zip(source.keys(), source.values()):
+            converted_value = set(map(map_to_real, value))
+            ckey = mapper[key]
+            target[ckey] = converted_value
+    
+    test_pairs = (
+        (converted_firsts, test_data_one.first_set_test_ans),
+        (converted_follows, test_data_one.follow_set_test_ans)
+    )
+    for ret, ans in test_pairs:
+        for key in ans.keys():
+            assert ret.get(key) == ans.get(key), key
+    
