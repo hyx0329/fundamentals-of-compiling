55152e75c79ee358b9f4925543d601e3c20f2de5
hyx0329 <hyx0329@163.com>
2021-07-03 23:59:48 +0800

    minor changes

--- semantic_example.py
+++ semantic_example.py
@@ -3,9 +3,9 @@ from analyzer.semantic.example import ArithmeticAnalysis
 
 if __name__ == "__main__":
     ath = ArithmeticAnalysis()
-
+    print("Support: *,+,brackets,negative numbers")
     while True:
-        string = input("Your expression(*,+, no priority, empty to quit):")
+        string = input("Your expression(empty to quit):")
         if len(string) == 0:
             print("Exit!")
             break
