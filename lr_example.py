a0ce6e7c1e23cbbfbbbc4cab280418b4b6e6a736
hyx0329 <hyx0329@163.com>
2021-07-03 16:35:27 +0800

    fix slr example

--- lr_example.py
+++ lr_example.py
@@ -1,8 +1,5 @@
 from analyzer.grammar.parser.lr import SLRAutomation
-from tests.data_to_test import ParserTestData as test_data_one
-from tests.data_to_test_ll import LLTestData as test_data_two
-from tests.data_to_test_ll import LLTestDataWithRecursion
-from tests.data_to_test_ll import ParserTestData
+from tests.data_to_test_lr import ParserTestData
 from tests.utils import print_table
 
 
@@ -43,4 +40,54 @@ def test_given_example():
 
 
 if __name__ == "__main__":
-    test_given_example()
+    rules = set()
+    symbols = set()
+
+    i = 1
+    while True:
+        rule = input('Your rule #{}(stop by empty input):'.format(i))
+        if len(rule) == 0:
+            break
+        i += 1
+        rules.add(rule)
+
+    start_state = input('Your initial state:')
+
+    syms = input("Your symbols(seperated by commas):")
+    symbols.update(syms.split(','))
+    symbols.difference_update(set(['']))
+    
+    slr_parser = SLRAutomation(rules, symbols, start_state)
+    mapper = slr_parser.parser.mapper
+    mapper_length = len(mapper)
+    parsing_table = slr_parser.parsing_table
+
+    def map_to_real2(x):
+        if isinstance(x, int):
+            if x < 0:
+                return 'eps'
+            elif x < mapper_length:
+                return mapper[x]
+        return x
+
+    print("ID, non-terminal, and correspoding reduce count:")
+    for i, var in enumerate(slr_parser.reducable_items):
+        print(i, '\t', map_to_real2(var[0]), '\t', len(var[1]))
+
+    print('Parsing table:')
+    print_table(
+        list(range(len(slr_parser.parsing_table))), 
+        slr_parser.pindex,
+        slr_parser.parsing_table,
+        map_to_real2
+    )
+
+    while True:
+        string = input("Test string(empty to quit):")
+        if len(string) == 0:
+            break
+        result, sym = slr_parser.parse(string)
+        if result:
+            print('Good input!')
+        else:
+            print('Bad input at symbol "{}".'.format(sym))
